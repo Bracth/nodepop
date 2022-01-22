7d8c57538cec2cdf0385efd78675f15accc6c7bd
Bryan Florencio <bryanricardo.florencioabreu@gmail.com>
2022-01-22 21:48:00 +0100

    Agregamos lista de anuncios en el site

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,17 @@
 var express = require('express');
 var router = express.Router();
+const Anuncio = require("../models/Anuncio")
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', async function (req, res, next) {
+  try {
+  res.locals.anuncios = await Anuncio.find();
+  
   res.render('index', { title: 'Express' });
+  } catch (err) {
+    next(err);
+  }
+ 
 });
 
 module.exports = router;
