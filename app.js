aa9368fe2e6c5146754b6f6227271c07fce1764e
Bryan Florencio <bryanricardo.florencioabreu@gmail.com>
2022-04-14 14:37:08 +0200

    We set the i18n to have multiple languages

--- app.js
+++ app.js
@@ -1,51 +1,58 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+var createError = require("http-errors");
+var express = require("express");
+var path = require("path");
+var cookieParser = require("cookie-parser");
+var logger = require("morgan");
 const swaggerMiddleware = require("./lib/swaggerMiddleware");
+const i18n = require("./lib/i18nConfigure");
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var indexRouter = require("./routes/index");
+var usersRouter = require("./routes/users");
 
 var app = express();
 
 // Nos conectamos con MongoDB //
 
-require("./lib/connectMongoose.js") 
+require("./lib/connectMongoose.js");
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
 
-app.use(logger('dev'));
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 app.use("/api-docs", swaggerMiddleware);
+
+// Setup de i18n
+app.use(i18n.init);
 
 // Rutas del API //
 
 app.use("/api/anuncios", require("./routes/api/anuncios"));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+// Rutas de mi website //
+
+app.use("/", indexRouter);
+app.use("/users", usersRouter);
+app.use("/change-locale", require("./routes/change-locale"));
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  res.locals.error = req.app.get("env") === "development" ? err : {};
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  res.render("error");
 });
 
 module.exports = app;
